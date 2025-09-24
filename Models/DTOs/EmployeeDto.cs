05febee0cd5525287bd0bad646c104a7303d090a
deepakgithub2020 <deepak,mld@gmail.com>
2025-09-24 16:45:10 +0530

    COmm new chanbt

--- Models/DTOs/EmployeeDto.cs
+++ Models/DTOs/EmployeeDto.cs
@@ -1,10 +1,16 @@
-﻿namespace Employee_Skills.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Skills.Models.DTOs
 {
     public class EmployeeDto
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public List<int> SkillIds { get; set; } 
     }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,10 +1,17 @@
-﻿namespace Employee_Skills.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Employee_Skills.Models
 {
     public class Employee
     {
+        [Key]
         public int Id { get; set; }
+        [Required, MaxLength(100)]        
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public ICollection<EmployeeSkill> EmployeeSkills { get; set; }
     }
 }
